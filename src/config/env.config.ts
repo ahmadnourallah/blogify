44c93298394f2205196de0de06d4012ff8b0ce76
Ahmad Nourallah <ahmadnurallah@gmail.com>
2026-01-08 08:44:01 -0500

    Fix type errors

--- src/config/env.config.ts
+++ src/config/env.config.ts
@@ -1,4 +1,5 @@
 import { PrismaPg } from "@prisma/adapter-pg";
+import type { StringValue } from "ms";
 import dotenv from "dotenv";
 
 dotenv.config();
@@ -6,7 +7,8 @@ dotenv.config();
 export const PORT = Number(process.env.PORT) || 3000;
 export const DB_URL = process.env.DB_URL || "";
 export const JWT_SECRET = process.env.JWT_SECRET || "";
-export const JWT_EXPIRATION_TIME = process.env.JWT_EXPIRATION_TIME || "2d";
+export const JWT_EXPIRATION_TIME =
+	(process.env.JWT_EXPIRATION_TIME as StringValue) || "2d";
 export const ALLOWED_ORIGINS = JSON.parse(process.env.ALLOWED_ORIGINS || "[]");
 export const THUMBNAILS_PATH = process.env.THUMBNAILS_PATH || "";
 export const ADAPTER = new PrismaPg({ connectionString: DB_URL });
--- src/utils/crypto.ts
+++ src/utils/crypto.ts
@@ -2,20 +2,14 @@ import { JWT_EXPIRATION_TIME, JWT_SECRET } from "../config/env.config";
 import type { User } from "~/prisma/generated/client";
 import jwt from "jsonwebtoken";
 
-function issueJWT(user: User) {
+function issueJWT(user: Omit<User, "password" | "bio">) {
 	const id = user.id;
 	const expiresIn = JWT_EXPIRATION_TIME;
-	const payload = {
-		id,
-		iat: Date.now(),
-	};
+	const payload = { id, iat: Date.now() };
 
 	const token = jwt.sign(payload, JWT_SECRET, { expiresIn });
 
-	return {
-		token,
-		expiresIn,
-	};
+	return { token, expiresIn };
 }
 
 export { issueJWT };
